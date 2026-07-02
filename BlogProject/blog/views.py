695c8cdc474f2e5086bd7aa4620bfc6ef16b7231
Your Name <your_email@example.com>
2026-07-02 18:52:54 +0900

    Implement basic view functions and routing for Exercise 6. Closes #2

--- BlogProject/blog/views.py
+++ BlogProject/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("The Blogs へようこそ！")
+
+
+def article_list(request):
+    titles = [
+        "Django入門: モデルとマイグレーション",
+        "URLとViewの配線を理解する",
+        "テンプレートで記事一覧を表示する",
+    ]
+    return HttpResponse("<br>".join(titles))
+
+
+def article_detail(request, article_id):
+    return HttpResponse(f"記事ID: {article_id} の詳細表示")
